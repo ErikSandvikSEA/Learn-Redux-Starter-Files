84a5a942d10096bfc28a43c72582b87a1d1a1b64
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-06-13 18:09:26 -0700

    step 11

---
+++ learn-redux/client/components/App.js
@@ -0,0 +1,18 @@
+import { bindActionCreators } from 'redux'
+import { connect  } from 'react-redux'
+import * as actionCreators from '../actions/actionCreators'
+
+import Main from './Main'
+
+function mapStateToProps(state){
+     return {
+          posts: state.posts,
+          comments: state.comments
+     }
+}
+
+function mapDistpatchToProps(dispatch) {
+     return bindActionCreators(actionCreators, dispatch)
+}
+
+const App = connect(mapStateToProps, mapDistpatchToProps)(Main)
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -4,5 +4,27 @@ import { render } from 'react-dom'
 //import css
 import css from './styles/style.styl'
 
-render(<p>hello</p>, document.getElementById('root'))
+//import components
+import App from './components/Main'
+import Single from './components/Single'
+import PhotoGrid from './components/PhotoGrid'
+
+//import react router dependencies
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Provider } from 'react-redux'
+import store, { history } from './store'
+
+const router = (
+     <Provider store={store}>
+          <Router history={history}>
+               <Route path='/' component={Main}>
+                    <IndexRoute component={PhotoGrid}></IndexRoute>
+                    <Route path='/view/:postId' component={Single}></Route>
+               </Route>
+          </Router>
+     </Provider>
+)
+
+
+render(router, document.getElementById('root'))
 
